6d4c38bcab621eab5f2a21e65d1ee5b3a053221d
Артём Кобылинский <art_kob@mail.ru>
2025-10-06 13:00:37 +0300

    больше исключений и проверок ввода

--- Program.cs
+++ Program.cs
@@ -6,37 +6,57 @@ while (true)
     {
 
         Console.WriteLine("Введите первое число:");
-        int a = int.Parse(Console.ReadLine());
-        Console.WriteLine("Выберите арифметическую опреацию + - * /");
-        char o = char.Parse(Console.ReadLine());
-        Console.WriteLine("Введите второе число:");
-        int b = int.Parse(Console.ReadLine());
-
-        switch (o)
+        string inputa = Console.ReadLine();
+        if (!double.TryParse(inputa, out double a))
         {
-            case '+':
-                Console.WriteLine($"результат {a}{o}{b} = {a + b}");
-                break;
-            case '*':
-                Console.WriteLine($"результат {a}{o}{b} = {a * b}");
-                break;
-            case '/':
-                Console.WriteLine($"результат {a}{o}{b} = {a / b} и {a % b} в остатке");
-                break;
-            case '-':
-                Console.WriteLine($"результат {a}{o}{b} = {a - b}");
-                break;
-            default: throw new Exception("Выбран неверный знак операции");
+            Console.WriteLine("Ошибка: Введено некорректное число!");
+            continue;
         }
+
+        Console.WriteLine("Выберите арифметическую опреацию + - * /");
+        string inputo = Console.ReadLine();
+        if (string.IsNullOrEmpty(inputo) || inputo.Length != 1|| !"+-*/".Contains(inputo[0]))
+        {
+            Console.WriteLine("Ошибка: Операция должна быть одним из символов + - * /");
+            continue;
+        }
+        char o = inputo[0];
+
+        Console.WriteLine("Введите второе число:");
+        string inputb = Console.ReadLine();
+        if (!double.TryParse(inputb, out double b))
+        {
+            Console.WriteLine("Ошибка: Введено некорректное число!");
+            continue;
+        }
+        double result = o switch
+        {
+            '+'=>a+b,
+            '*'=>a*b,
+            '-' => a - b,
+            '/' => b != 0 ? a / b : throw new DivideByZeroException(),
+            _ => throw new Exception("Выбран неверный знак операции")
+        };
+
+        if (double.IsInfinity(result))
+            throw new OverflowException("Результат операции слишком большой");
+        if (double.IsNaN(result))
+            throw new InvalidOperationException("Результат операции неопределен");
+
+        Console.WriteLine($"результат {a}{o}{b} = {result}");
     }
 
-    catch (FormatException)
-    {
-        Console.WriteLine("Ошибка: Введены некорректные данные!");
-    }
     catch (DivideByZeroException)
     {
         Console.WriteLine("Ошибка: Деление на ноль!");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("Ошибка: Результат операции слишком большой!");
+    }
+    catch (InvalidOperationException ex)
+    {
+        Console.WriteLine($"Ошибка: {ex.Message}");
     }
     catch (Exception ex)
     {
